68d90da553a4b9b47b1cf269afa8af8f6e14c2d1
jackbud13 <jackbud@yandex.ru>
2019-11-27 13:13:33 +0300

    m6t1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,29 +1,39 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
-     *          name: 'Ilia',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *      },
- *
- * @constructor
- */
+// Add your code here
+
+// 'use strict';
+
+
 class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
     this.data = data;
+    let tableContent = '<thead><tr class="result__tr result__tr--head">';
+    tableContent += '<td class="result__td result__td--head">Name</td>';
+    tableContent += '<td class="result__td result__td--head">Age</td>';
+    tableContent += '<td class="result__td result__td--head">Salary</td>';
+    tableContent += '<td class="result__td result__td--head">City</td>';
+    tableContent += '<td class="result__td result__td--head"> </td></tr></thead><tbody>';
+
+    this.data.forEach((key) => {
+      tableContent += `<tr id=${key.id} class="result__tr"><td class="result__td">${key.name}</td>`;
+      tableContent += `<td class="result__td result__td--num">${key.age}</td>`;
+      tableContent += `<td class="result__td result__td--num">${key.salary}</td>`;
+      tableContent += `<td class="result__td">${key.city}</td>`;
+      tableContent += '<td class="result__td result__td--del"><a href="#delete" class="result__a">X</a></td></tr>';
+    });
+    tableContent += '</tbody>';
+    this.el.insertAdjacentHTML('beforeend', tableContent);
+    // console.log(tableContent);
   }
 
   /**
-   * Метод который выщывается после удалении строки
+   * Метод который вызывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
-  onRemoved(id) {}
+  onRemoved(id) {
+    const tr = document.getElementById(id);
+    tr.remove();
+    console.log(`Из таблицы удален пользователь ${id}`);
+  }
+  // if (!td) {}
 }
-
-window.ClearedTable = ClearedTable;
